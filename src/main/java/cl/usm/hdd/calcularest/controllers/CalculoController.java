f412cd41dafd61f11837bf28448d9a51715f9c3b
Kulvins <lucho.441@hotmail.com>
2023-10-06 14:50:44 -0300

    commit inicial con funciones y control de errores

---
+++ src/main/java/cl/usm/hdd/calcularest/controllers/CalculoController.java
@@ -0,0 +1,33 @@
+package cl.usm.hdd.calcularest.controllers;
+
+import cl.usm.hdd.calcularest.entities.CalculoRequest;
+import cl.usm.hdd.calcularest.entities.CalculoResponse;
+import cl.usm.hdd.calcularest.exceptions.OperacionException;
+import cl.usm.hdd.calcularest.services.CalculoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CalculoController {
+    @Autowired
+    private CalculoService calculoService;
+
+    @PostMapping("/calcular")
+    public ResponseEntity<CalculoResponse> calcular(@RequestBody CalculoRequest calculoRequest){
+        try {
+            double resultado = calculoService.calcular(calculoRequest.getNum1()
+                    , calculoRequest.getNum2(), calculoRequest.getOperacion());
+            CalculoResponse calculoResponse = new CalculoResponse();
+            calculoResponse.setResultado(resultado);
+
+            return ResponseEntity.ok(calculoResponse);
+        }catch(OperacionException ex){
+            return ResponseEntity.badRequest().body(null);
+        }catch(Exception diego){
+            return ResponseEntity.internalServerError().body(null);
+        }
+    }
+}
